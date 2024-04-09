86967027cfc67b9474a0bf20f0a69ec54c4f5b0f
Jhonny0102 <fjhon868@gmail.com>
2024-04-09 14:20:05 +0200

    Add project files.

---
+++ ApiCrudCoreDoctores Client/Controllers/DoctorController.cs
@@ -0,0 +1,26 @@
+﻿using ApiCrudCoreDoctores_Client.Models;
+using ApiCrudCoreDoctores_Client.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCrudCoreDoctores_Client.Controllers
+{
+    public class DoctorController : Controller
+    {
+        private ServiceDoctor service;
+        public DoctorController(ServiceDoctor service)
+        {
+            this.service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Doctor> doctores = await this.service.GetDoctoresAsync();
+            return View(doctores);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            Doctor doctor = await this.service.FindDoctorAsync(id);
+            return View(doctor);
+        }
+    }
+}
---
+++ ApiCrudCoreDoctores Client/Services/ServiceDoctor.cs
@@ -0,0 +1,50 @@
+﻿using ApiCrudCoreDoctores_Client.Models;
+using System.Net.Http.Headers;
+
+namespace ApiCrudCoreDoctores_Client.Services
+{
+    public class ServiceDoctor
+    {
+        private string UrlApi;
+        private MediaTypeWithQualityHeaderValue header;
+        public ServiceDoctor(IConfiguration configuration)
+        {
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+            this.UrlApi = configuration.GetValue<string>("ApiUrls:ApiDoctor");
+        }
+
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                HttpResponseMessage response = await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+        public async Task<List<Doctor>> GetDoctoresAsync()
+        {
+            string request = "api/doctor";
+            List<Doctor> data = await CallApiAsync<List<Doctor>>(request);
+            return data;
+        }
+
+        public async Task<Doctor> FindDoctorAsync(int id)
+        {
+            string request = "api/doctor/"+id;
+            Doctor data = await CallApiAsync<Doctor>(request);
+            return data;
+        }
+    }
+}
